acd9df934d4e63ed9678071639c9313fb417b316
Hao.Tu <hao.tu@alta.com.vn>
2019-02-02 11:41:14 +0700

    add preload,chang scene function

---
+++ Assets/Hao.Tu/Scirpt/GameController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public static GameController m_GameControler;
+
+    [SerializeField]
+    private List<GameObject> m_PanelList = new List<GameObject>();
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void ChangeScene(int _index)
+    {
+        
+        for (int i = 0; i < m_PanelList.Count; i++)
+        {
+                m_PanelList[i].SetActive(false);
+        }
+        m_PanelList[_index].SetActive(true);
+    }
+
+}
+public enum PanelNameEnum
+{
+    MainMenu,
+    AddPlayer
+}
+
---
+++ Assets/Hao.Tu/Scirpt/Preload.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Preload : MonoBehaviour {
+
+    public static Preload m_Instance;
+    [SerializeField]
+    private LoadingScene m_LoadingScene;
+
+	// Use this for initialization
+	void Start () {
+        m_Instance = this;
+        m_LoadingScene.enabled = true;
+
+    }
+	
+
+}
